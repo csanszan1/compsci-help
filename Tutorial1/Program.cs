d4e31ef1ae4fcc4879d29cb891ffb233932c18bd
Csanad Ablonczy <csanad@acsanad.eu>
2025-01-11 18:40:40 +0100

    initial commit

---
+++ Tutorial1/Program.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Data.Sqlite;
+
+using var connection = new SqliteConnection("Data Source=D:\\Source\\Repos\\Tutorial1\\asd.db");
+connection.Open();
+
+using var command = connection.CreateCommand();
+command.CommandText = "SELECT * FROM users";
+
+using var reader = command.ExecuteReader();
+while (reader.Read())
+{
+    var username = reader.GetString(1);
+    Console.WriteLine(username);
+}
